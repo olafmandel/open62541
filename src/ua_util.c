cf26a5fb679b9e018b8ad289d84f5da89f7b511a
Stefan Profanter <git@s.profanter.me>
2017-08-02 10:06:38 +0200

    Add DUMP_HEX_PKG function to allow easier fuzz debugging

--- src/ua_util.c
+++ src/ua_util.c
@@ -86,3 +86,33 @@ UA_parseEndpointUrl(const UA_String *endpointUrl, UA_String *outHostname,
 
     return UA_STATUSCODE_GOOD;
 }
+
+
+#ifdef UA_DUMP_HEX_PKG
+#include <ctype.h>
+#include <stdio.h>
+void UA_dump_hex_pkg(UA_Byte* buffer, size_t bufferLen) {
+    printf("--------------- HEX Package Start ---------------\n");
+    char ascii[17];
+    memset(ascii,0,17);
+    for (size_t i = 0; i < bufferLen; i++)
+    {
+        if (i == 0)
+            printf("%08zx ", i);
+        else if (i%16 == 0)
+            printf("|%s|\n%08zx ", ascii, i);
+        if (isprint((int)(buffer[i])))
+            ascii[i%16] = (char)buffer[i];
+        else
+            ascii[i%16] = '.';
+        printf("%02X ", (unsigned char)buffer[i]);
+    }
+    size_t fillPos = bufferLen %16;
+    ascii[fillPos] = 0;
+    for (size_t i=fillPos; i<16; i++) {
+        printf("   ");
+    }
+    printf("|%s|\n%08zx\n", ascii, bufferLen);
+    printf("--------------- HEX Package END ---------------\n");
+}
+#endif
